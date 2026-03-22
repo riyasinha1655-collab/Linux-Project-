11268f86d107794388044a24c04a8277c3739814
riyasinha1655-collab <riyasinha1655@gmail.com>
2026-03-22 17:58:14 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+
+for DIR in "${DIRS[@]}"; do
+if [ -d "$DIR" ]; then
+PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+echo "$DIR => $PERMS | Size: $SIZE"
+else
+echo "$DIR does not exist"
+fi
+done
